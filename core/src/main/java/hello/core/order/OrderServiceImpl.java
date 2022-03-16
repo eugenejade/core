2a8c4283215fa986c8887eb7639bb99ace55811c
eugene <eugene_jade@dkitec.com>
2022-03-17 01:10:39 +0900

    appConfig 적용

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,12 +9,19 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 	
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	private final MemberRepository memberRepository;
 	
+	private DiscountPolicy discountPolicy;
+	
+	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+		this.memberRepository = memberRepository;
+		this.discountPolicy = discountPolicy;
+	}
+
 	//private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 	//private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 	
-	private DiscountPolicy discountPolicy;
+	
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
